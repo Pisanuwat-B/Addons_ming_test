e4672157cfedf6471c1d43ec9f6816330727e873
winyook <winyoo@tinnakorn.com>
2016-07-01 01:45:01 +0700

    [IMP] Improve inherite purchase_order_line and sale_order_line using 'super' 	modified:   winyoo_po/purchase.py 	modified:   winyoo_sale/sale.py

--- winyoo_sale/sale.py
+++ winyoo_sale/sale.py
@@ -59,135 +59,51 @@ class sale_order(models.Model):
         ctx=dict(self._context)
         return self.with_context(ctx).write({'state':'sent'})
     
-    
-    @api.multi
-    def button_mycancel(self):
-        ctx=dict(self._context)
-        return self.with_context(ctx).write({'state':'cancel'})
- 
-    
+#     @api.multi
+#     def button_mycancel(self):
+#         ctx=dict(self._context)
+#         return self.with_context(ctx).write({'state':'cancel'})
     
 
-class sale_order_line(models.Model):
+class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     _order = 'order_id asc, sequence, id'   
-
-# แก้ให้ Description ใส่เฉพาะที่เราต้องการเพิ่ม ไม่ต้องใส่ชื่อ มาด้วย แล้วชื่อก็บังคับ เปลี่ยนไม่ได้
+  
     name = fields.Text('Description รายละเอียดใต้ชื่อสินค้า', 
-            required=False, default=' ', readonly=True, states={'draft': [('readonly', False)]},
+            required=True, default=' ', readonly=True, states={'draft': [('readonly', False)]},
             help="รายละเอียดใต้ชื่อสินค้าในใบเสนอราคาและใบดีโอ เช่น สินค้าน้ำมันพืชปทุม หรือ สินค้าทีโอแอล หรือ ราคาสำหรับขั้นต่ำ 100 kg")
     #'name': fields.text('Description', required=True, readonly=True, states={'draft': [('readonly', False)]}),
-    
-    def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
-            uom=False, qty_uos=0, uos=False, name=' ', partner_id=False,
-            lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, context=None):
-        context = context or {}
-        lang = lang or context.get('lang', False)
-        if not partner_id:
-            raise osv.except_osv(_('No Customer Defined!'), _('Before choosing a product,\n select a customer in the sales form.'))
-        warning = False
-        product_uom_obj = self.pool.get('product.uom')
-        partner_obj = self.pool.get('res.partner')
-        product_obj = self.pool.get('product.product')
-        context = {'lang': lang, 'partner_id': partner_id}
-        partner = partner_obj.browse(cr, uid, partner_id)
-        lang = partner.lang
-        context_partner = {'lang': lang, 'partner_id': partner_id}
+    print "1SaleOrderLine++++++++++++++++++++++++++++++++++++++++++++++++++++"       
+    @api.multi
+    def product_id_change(
+            self, pricelist, product, qty=0, uom=False, qty_uos=0,
+            uos=False, name='', partner_id=False, lang=False,
+            update_tax=True, date_order=False, packaging=False,
+            fiscal_position=False, flag=False
+    ):
+        res = super(SaleOrderLine, self).product_id_change(
+            pricelist=pricelist, product=product, qty=qty, uom=uom,
+            qty_uos=qty_uos, uos=uos, name=name,
+            partner_id=partner_id, lang=lang, update_tax=update_tax,
+            date_order=date_order, packaging=packaging,
+            fiscal_position=fiscal_position, flag=flag)
+        print "2SaleOrderLine+++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        print "name: ", name
+        print "product: ", product
+        if product:
+            product_obj = self.env['product.product']
+            product = product_obj.browse(product)
+            #name =' '
+            print "product2: ", product
+            print "product.description_sale", product.description_sale
+            if 'value' not in res:
+                res['value'] = {}
+            if product.description_sale:
+                res['value']['name'] = product.description_sale
+            if not product.description_sale:    
+               res['value']['name'] = ' '             
+        return res
 
-        if not product:
-            return {'value': {'th_weight': 0,
-                'product_uos_qty': qty}, 'domain': {'product_uom': [],
-                   'product_uos': []}}
-        if not date_order:
-            date_order = time.strftime(DEFAULT_SERVER_DATE_FORMAT)
-
-        result = {}
-        warning_msgs = ''
-        product_obj = product_obj.browse(cr, uid, product, context=context_partner)
-
-        uom2 = False
-        if uom:
-            uom2 = product_uom_obj.browse(cr, uid, uom)
-            if product_obj.uom_id.category_id.id != uom2.category_id.id:
-                uom = False
-        if uos:
-            if product_obj.uos_id:
-                uos2 = product_uom_obj.browse(cr, uid, uos)
-                if product_obj.uos_id.category_id.id != uos2.category_id.id:
-                    uos = False
-            else:
-                uos = False
-
-        fpos = False
-        if not fiscal_position:
-            fpos = partner.property_account_position or False
-        else:
-            fpos = self.pool.get('account.fiscal.position').browse(cr, uid, fiscal_position)
-        if update_tax: #The quantity only have changed
-            result['tax_id'] = self.pool.get('account.fiscal.position').map_tax(cr, uid, fpos, product_obj.taxes_id)
-
-        if not flag:
-            result['name'] = ' ' #self.pool.get('product.product').name_get(cr, uid, [product_obj.id], context=context_partner)[0][1]
-            if product_obj.description_sale:
-                result['name'] += product_obj.description_sale
-        domain = {}
-        if (not uom) and (not uos):
-            result['product_uom'] = product_obj.uom_id.id
-            if product_obj.uos_id:
-                result['product_uos'] = product_obj.uos_id.id
-                result['product_uos_qty'] = qty * product_obj.uos_coeff
-                uos_category_id = product_obj.uos_id.category_id.id
-            else:
-                result['product_uos'] = False
-                result['product_uos_qty'] = qty
-                uos_category_id = False
-            result['th_weight'] = qty * product_obj.weight
-            domain = {'product_uom':
-                        [('category_id', '=', product_obj.uom_id.category_id.id)],
-                        'product_uos':
-                        [('category_id', '=', uos_category_id)]}
-        elif uos and not uom: # only happens if uom is False
-            result['product_uom'] = product_obj.uom_id and product_obj.uom_id.id
-            result['product_uom_qty'] = qty_uos / product_obj.uos_coeff
-            result['th_weight'] = result['product_uom_qty'] * product_obj.weight
-        elif uom: # whether uos is set or not
-            default_uom = product_obj.uom_id and product_obj.uom_id.id
-            q = product_uom_obj._compute_qty(cr, uid, uom, qty, default_uom)
-            if product_obj.uos_id:
-                result['product_uos'] = product_obj.uos_id.id
-                result['product_uos_qty'] = qty * product_obj.uos_coeff
-            else:
-                result['product_uos'] = False
-                result['product_uos_qty'] = qty
-            result['th_weight'] = q * product_obj.weight        # Round the quantity up
-
-        if not uom2:
-            uom2 = product_obj.uom_id
-        # get unit price
-
-        if not pricelist:
-            warn_msg = _('You have to select a pricelist or a customer in the sales form !\n'
-                    'Please set one before choosing a product.')
-            warning_msgs += _("No Pricelist ! : ") + warn_msg +"\n\n"
-        else:
-            price = self.pool.get('product.pricelist').price_get(cr, uid, [pricelist],
-                    product, qty or 1.0, partner_id, {
-                        'uom': uom or result.get('product_uom'),
-                        'date': date_order,
-                        })[pricelist]
-            if price is False:
-                warn_msg = _("Cannot find a pricelist line matching this product and quantity.\n"
-                        "You have to change either the product, the quantity or the pricelist.")
-
-                warning_msgs += _("No valid pricelist line found ! :") + warn_msg +"\n\n"
-            else:
-                result.update({'price_unit': price})
-        if warning_msgs:
-            warning = {
-                       'title': _('Configuration Error!'),
-                       'message' : warning_msgs
-                    }
-        return {'value': result, 'domain': domain, 'warning': warning} 
     
     
     
